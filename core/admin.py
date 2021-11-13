9652ecfa932b936952539fd0d468005341162a7d
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-11-13 14:08:49 -0300

    [lint] correçao de erros

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Cliente, Empresa, Lance, Oferta
 
 # Register your models here.
 
 admin.site.register([
-    Empresa, 
+    Empresa,
     Cliente,
     Oferta,
     Lance
