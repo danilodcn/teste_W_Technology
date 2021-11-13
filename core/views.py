d1fa3e8c2cc553fa8bdd693ecaa58f7a1c4a2573
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-11-13 13:57:40 -0300

    ciraçao das views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,41 @@
-# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+import serializers
+import models
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint usado para gerenciar o model Client
+    """
+    queryset = models.Cliente.objects.all()
+    serializer_class = serializers.ClientSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+
+class EmpresaViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint usado para gerenciar o model Empresa
+    """
+    queryset = models.Empresa.objects.all()
+    serializer_class = serializers.EmpresaSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+
+class OfertaViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint usado para gerenciar o model Oferta
+    """
+    queryset = models.Oferta.objects.all()
+    serializer_class = serializers.OfertaSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+
+class LanceViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint usado para gerenciar o model Lance
+    """
+    queryset = models.Lance.objects.all()
+    serializer_class = serializers.LanceSerializer
+    permission_class = [permissions.IsAuthenticated]
